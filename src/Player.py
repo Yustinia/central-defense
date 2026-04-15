1ee54f689ada73b387ffa72385641882bcb6a6ec
Yustinia <yustinia@protonmail.com>
2026-04-15 17:34:57 +0800

    slightly adjusted player death req

--- src/Player.py
+++ src/Player.py
@@ -73,8 +73,7 @@ class Player(BoxEntity):
 
         self.health -= amount
 
-        if self.health <= 0 or self.health == 0:
-            self.health = 0
+        if self.health <= 10 or self.health <= 0:
             self.is_alive = False
 
         return self.is_alive
